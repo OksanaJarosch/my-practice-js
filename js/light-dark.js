451e151e75ec8609bc17b02f57cad6632cf8ecf2
Oksana Jarosch <ok.jarosch@gmail.com>
2023-10-15 23:16:47 +0200

    pagination-for-project

--- js/light-dark.js
+++ js/light-dark.js
@@ -14,3 +14,35 @@ function onToggle(evt) {
     }
     
 }
+
+
+let parsedLocalData;
+
+parsedLocalData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//---------pagination-----------
+
+let pages = Math.ceil(parsedLocalData.length / 3);
+console.log(pages);
+     
+const buttonsContainer = document.querySelector(".pagination-container");
+
+buttonsContainer.addEventListener("click", onButtons);
+
+// buttonsContainer.classList.add("is-hidden");
+
+function onButtons(evt) {
+if (pages === 0) {
+  buttonsContainer.classList.add("is-hidden");
+} else {
+  buttonsContainer.classList.remove("is-hidden");
+}
+
+  // if (evt.target.textContent === "1") {
+  //   console.log("Im on first page");
+      
+  // } else if (evt.target.textContent === "2") {
+    
+  // }
+}
+
