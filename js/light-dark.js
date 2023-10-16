2f83cd100efa7ddd9dbf8194222855f47bdcedef
Oksana Jarosch <ok.jarosch@gmail.com>
2023-10-16 22:54:48 +0200

    pages-buttons-js

--- js/light-dark.js
+++ js/light-dark.js
@@ -18,31 +18,100 @@ function onToggle(evt) {
 
 let parsedLocalData;
 
-parsedLocalData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+parsedLocalData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//---------pagination-----------
-
-let pages = Math.ceil(parsedLocalData.length / 3);
-console.log(pages);
-     
+//---------PAGINATION-----------
+   
 const buttonsContainer = document.querySelector(".pagination-container");
-
 buttonsContainer.addEventListener("click", onButtons);
 
-// buttonsContainer.classList.add("is-hidden");
+const contentPagConteiner = document.querySelector(".js-content-container");
 
-function onButtons(evt) {
+//Buttons
+const firstBtn = document.querySelector(".btn-first");
+const secondBtn = document.querySelector(".btn-second");
+const thirdBtn = document.querySelector(".btn-third");
+const moreBtn = document.querySelector(".btn-more");
+
+
+let pages = Math.ceil(parsedLocalData.length / 4);
+console.log(pages);
+
+let currentPage = 1;
+
+let step = 0;
+
+let onePage = parsedLocalData.slice(step, step + 4);
+
+//---------markup-----------
+
+function makeMarkup(arr) {
+ const markup = arr.map(item => {
+    return `<div>${item}</div>`
+  }).join("");
+
+  contentPagConteiner.innerHTML = markup;
+}
+
 if (pages === 0) {
   buttonsContainer.classList.add("is-hidden");
 } else {
-  buttonsContainer.classList.remove("is-hidden");
+makeMarkup(onePage)
 }
 
-  // if (evt.target.textContent === "1") {
-  //   console.log("Im on first page");
-      
-  // } else if (evt.target.textContent === "2") {
-    
-  // }
+if (pages === 1) {
+  secondBtn.disabled = true;
+  moreBtn.disabled = true;
+  thirdBtn.disabled = true;
 }
+if (pages === 2) {
+  thirdBtn.disabled = true;
+  moreBtn.disabled = true;
+}
+if (+(secondBtn.textContent) === pages) {
+  moreBtn.disabled = true;
+}
+
+
+//---------on button-----------
+
+function onButtons(evt) {
+
+  // if (pages === 0) {
+  //   buttonsContainer.classList.add("is-hidden");
+  // } else {
+  //   buttonsContainer.classList.remove("is-hidden");
+  // }
+  const click = evt.target;
+
+  if (click.classList.contains("btn-first")) {
+    currentPage = click.textContent;
+  }
+
+  if (click.classList.contains("btn-second")) {
+    currentPage = click.textContent;
+    console.log("Hi");
+  }
+  
+  if (click.classList.contains("btn-third")) {
+    currentPage = click.textContent;
+  } click
+  
+  if (click.classList.contains("btn-more")) {
+      currentPage = click.textContent;
+      firstBtn.textContent = +(firstBtn.textContent) + 1;
+      secondBtn.textContent = +(secondBtn.textContent) + 1;
+    thirdBtn.textContent = +(thirdBtn.textContent) + 1;
+    
+        if (+(secondBtn.textContent) === pages) {
+      moreBtn.disabled = true;
+    }
+    }
+  
+    step = 0 + 4 * (currentPage - 1);
+    onePage = parsedLocalData.slice(step, step + 4);
+    makeMarkup(onePage);
+    
+  }
+
 
