7b51346ba61ced2c5eba78d74a19dba14c0b0c6e
Oksana Jarosch <ok.jarosch@gmail.com>
2023-10-16 23:48:25 +0200

    forward-buttons-js

--- js/light-dark.js
+++ js/light-dark.js
@@ -18,7 +18,7 @@ function onToggle(evt) {
 
 let parsedLocalData;
 
-parsedLocalData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+parsedLocalData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 //---------PAGINATION-----------
    
@@ -32,6 +32,10 @@ const firstBtn = document.querySelector(".btn-first");
 const secondBtn = document.querySelector(".btn-second");
 const thirdBtn = document.querySelector(".btn-third");
 const moreBtn = document.querySelector(".btn-more");
+const backBtn = document.querySelector(".btn-back");
+const oneBackBtn = document.querySelector(".btn-one-back");
+const oneForwardBtn = document.querySelector(".btn-one-forward");
+const forwardBtn = document.querySelector(".btn-forward");
 
 
 let pages = Math.ceil(parsedLocalData.length / 4);
@@ -82,31 +86,84 @@ function onButtons(evt) {
   // } else {
   //   buttonsContainer.classList.remove("is-hidden");
   // }
+
   const click = evt.target;
 
   if (click.classList.contains("btn-first")) {
     currentPage = click.textContent;
+    secondBtn.classList.remove("btn-activ");
+    firstBtn.classList.add("btn-activ");
+    moreBtn.classList.remove("btn-activ");
+
+    if (firstBtn !== pages -2) {
+      oneForwardBtn.disabled = false;
+      forwardBtn.disabled = false;
+    }
   }
 
   if (click.classList.contains("btn-second")) {
     currentPage = click.textContent;
-    console.log("Hi");
+    secondBtn.classList.add("btn-activ");
+    firstBtn.classList.remove("btn-activ");
+    moreBtn.classList.remove("btn-activ");
+
+    if (secondBtn !== pages - 1) {
+      oneForwardBtn.disabled = false;
+      forwardBtn.disabled = false;
+    }
   }
   
-  if (click.classList.contains("btn-third")) {
-    currentPage = click.textContent;
-  } click
+  // if (click.classList.contains("btn-third")) {
+  //   currentPage = click.textContent;
+  // } 
   
   if (click.classList.contains("btn-more")) {
+
+    if (+(secondBtn.textContent) === pages - 1) {
+      moreBtn.textContent = pages;
       currentPage = click.textContent;
+      moreBtn.classList.add("btn-activ");
+      firstBtn.classList.remove("btn-activ");
+      secondBtn.classList.remove("btn-activ");
+
+      oneForwardBtn.disabled = true;
+      forwardBtn.disabled = true;
+      
+    } else {
       firstBtn.textContent = +(firstBtn.textContent) + 1;
       secondBtn.textContent = +(secondBtn.textContent) + 1;
-    thirdBtn.textContent = +(thirdBtn.textContent) + 1;
-    
-        if (+(secondBtn.textContent) === pages) {
-      moreBtn.disabled = true;
     }
+  }
+  
+  if (click === oneForwardBtn) {
+
+    if (+(secondBtn.textContent) === pages - 2) {
+      moreBtn.textContent = pages;
+      currentPage = moreBtn.textContent;
+      moreBtn.classList.add("btn-activ");
+      firstBtn.classList.remove("btn-activ");
+      secondBtn.classList.remove("btn-activ");
+
+      oneForwardBtn.disabled = true;
+      forwardBtn.disabled = true;
+      
+      firstBtn.textContent = +(firstBtn.textContent) + 1;
+      secondBtn.textContent = +(secondBtn.textContent) + 1;
     }
+  }
+      
+    if (click === forwardBtn) {
+      firstBtn.textContent = pages - 2;
+      secondBtn.textContent = pages - 1;
+      moreBtn.textContent = pages;
+      currentPage = moreBtn.textContent;
+      moreBtn.classList.add("btn-activ");
+      firstBtn.classList.remove("btn-activ");
+      secondBtn.classList.remove("btn-activ");
+  
+      oneForwardBtn.disabled = true;
+      forwardBtn.disabled = true;
+  } 
   
     step = 0 + 4 * (currentPage - 1);
     onePage = parsedLocalData.slice(step, step + 4);
