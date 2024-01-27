f7bf0b11f18f8c67d39ed9eb54b16802b053806a
Oksana Jarosch <ok.jarosch@gmail.com>
2024-01-28 00:14:59 +0100

    eloquentjavascript, chapter3

--- js/script2.js
+++ js/script2.js
@@ -45,3 +45,41 @@
 // }
 
 // console.log(chessboard);
+
+
+//? Book exercises https://eloquentjavascript.net/03_functions
+
+// function min(a,b) {
+//     return (a < b) ? a : b;
+// }
+// console.log(min(0, 10));
+// console.log(min(0, -10));
+
+
+//? Use recursion
+
+// function isEven(n) {
+//     if (n === 0) {
+//         return "true"
+//     } else if (n === 1) {
+//         return "false"
+//     } else if (n < 0) {
+//         return isEven(n + 2);
+//     }
+//     return isEven(n - 2);
+// }
+// console.log(isEven(50));
+// console.log(isEven(75));
+// console.log(isEven(-1));
+
+
+// function countChar(string, char) {
+//     let counter = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] === char){
+//             counter += 1;
+//         }
+//     }
+//     return counter;
+// }
+// console.log(countChar("kakkerlak", "k"));
